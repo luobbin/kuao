a8a6d57bb6c178ac4c14e7187e4bdcc42af15c2e
robin <157908152@qq.com>
2022-07-25 18:08:25 +0800

    新版样式-关于我们

--- resources/views/articles/test.about_us.blade.php
+++ resources/views/articles/test.about_us.blade.php
@@ -23,49 +23,22 @@
 <!-- 内页banner End -->
 <div class="clearfix"></div>
 
-<div class="news w83">
+<div class="news w83 p110">
     <!--开始:库奥照明简介-->
-    <div class="about">
-        <div class="col-top block-title">
-            <div class="tit-ban">库奥照明简介</div>
-            <div class="fr">
-                <a href="#about">公司介绍</a>
-                <a href="#about1">发展历程</a>
-                <a href="#about2">服务领域</a>
-                <a href="#about4">关联案例分类</a>
-            </div>
-        </div>
-        <div class="clearfix"></div>
-        <div id="about"></div>
-        <div class="col-middle" style="background-color: #EFEFEF;float: left;padding: 50px 0;">
-            <div class="info">
-                <div class="limg">
-                    <img src="/static/img_topic/about_usinfo_01.jpg">
-                </div>
-                <div class="rtxt">
-                    <h3>库奥（深圳）照明技术有限公司</h3>
-                    <p>
-                        库奥（深圳）照明技术有限公司一直致力于国际前沿技术的研究与创新，研发出的SUBROSA隐光、LiteBrush光瀑、LiteBox偏光洗墙等核心技术，广泛运用到博物馆照明产品中，相继完成了故宫博物院、良渚博物院、南京牛首山、曲阜尼山等重要文化圣地，用丰富的灯光灵感，照亮文物璀璨新生。凭借在高端展陈空间里的技术优势，如今我们在国内已服务了超过40%的国家一级博物馆，参与制定国家行业标准，以创新引领行业，以品质树立标杆。
-                    </p>
-                </div>
-            </div>
-
-            <div class="info">
-                <div class="ltxt">
-                    <p>
-                        专业且充满热情的服务团队，给予空间高级定制般的灯光体验。以深厚的技术底蕴，为博物馆、高端酒店住宅、商业综合体、零售、建筑，景观等空间提供可持续照明解决方案和技术应用。<br>
-                        秉承“行动本土化，思维全球化”的理念，库奥在国内拥有专属的研发中心及工厂，以创新的技术、卓越的品质和一流的服务，为多元空间创造非凡的灯光体验。以深厚的技术底蕴，为博物馆、高端酒店住宅、商业综合体、零售、建筑，景观等空间提供可持续照明解决方案和技术应用。
-                    </p>
-                </div>
-                <div class="rimg">
-                    <img src="/static/img_topic/about_usinfo_04.jpg">
-                </div>
-            </div>
-
+    <div class="block-title">
+        <div class="tit-ban">库奥照明简介</div>
+        <div class="fr">
+            <a href="#about">公司介绍</a>
+            <a href="#about1">发展历程</a>
+            <a href="#about2">服务领域</a>
+            <a href="#about4">关联案例分类</a>
         </div>
     </div>
     <div class="clearfix"></div>
-
+    <div id="about"></div>
+    <div style="background-color: #EFEFEF; margin-bottom: 80px;">
+        <img src="/static/img_topic/about_us01.png" style="width: 100%; margin: 50px 0;">
+    </div>
     <!--结束:库奥照明简介-->
     <!--开始:发展历程-->
     <div id="about1"></div>
