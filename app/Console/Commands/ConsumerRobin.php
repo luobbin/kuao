b0e1e7bfe4540bd2e649bbde84e6c1125e307361
robin <157908152@qq.com>
2022-08-01 10:02:32 +0800

    新版样式-资讯

--- app/Console/Commands/ConsumerRobin.php
+++ app/Console/Commands/ConsumerRobin.php
@@ -47,6 +47,14 @@ class ConsumerRobin extends Command
             $this->info("public_path:".public_path());
             $this->info("realpath:".realpath());
             echo microtime();
+        }elseif ($act=='foot_cate'){//测试底部菜单
+            $content = '[{"name":"\u5173\u4E8E\u6211\u4EEC","url":"%23","childs":[{"name":"\u5173\u4E8E\u5E93\u5965","url":"\/news\/about_us"},{"name":"\u5DE5\u5382\u4ECB\u7ECD","url":"\/news\/factory_info"}]},{"name":"\u4EA7\u54C1\u4E2D\u5FC3","url":"\/products","childs":[{"name":"\u5BA4\u5185\u4E13\u4E1A\u7C7B","url":"\/products?cid=1"},{"name":"\u5BA4\u5185\u8BBE\u8BA1\u7C7B","url":"\/products?cid=2"},{"name":"\u535A\u7269\u9986\u7167\u660E","url":"\/products?cid=3"},{"name":"\u5C55\u793A\u7BB1","url":"\/products?cid=4"}]},{"name":"\u9879\u76EE\u6848\u4F8B","url":"\/cases","childs":[{"name":"\u535a\u7269\u9986&\u7f8e\u672f\u9986&\u79d1\u6280\u9986","url":"\/cases?cate_id=1"},{"name":"\u661F\u7EA7\u9152\u5E97","url":"\/cases?cate_id=2"},{"name":"\u5BB6\u88C5\u522B\u5885","url":"\/cases?cate_id=3"},{"name":"\u5546\u4E1A\u7A7A\u95F4","url":"\/cases?cate_id=4"}]},{"name":"\u5173\u4E8E\u6211\u4EEC","url":"%23","childs":[{"name":"\u516C\u53F8\u8D44\u8BAF","url":"\/articles"},{"name":"\u8054\u7CFB\u6211\u4EEC","url":"\/news\/contact_us"}]}]';
+            $list = json_decode($content,true);
+            print_r($list);
+            $list[3]['name'] = "加入我们";
+            print_r($list);
+            echo json_encode($list);
+
         }elseif ($act=='test'){//测试
             $start = microtime();
             $waster = get_microtime_between($start,microtime());
