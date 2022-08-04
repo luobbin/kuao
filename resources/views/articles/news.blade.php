053c0f69e11095009e53326bbda2336741e24945
robin <157908152@qq.com>
2022-08-04 10:19:08 +0800

    新版样式-资讯

--- resources/views/articles/news.blade.php
+++ resources/views/articles/news.blade.php
@@ -12,15 +12,7 @@
 @endsection
 
 @section('footer_js')
-    <script>
-        window.sr = new ScrollReveal({
-            distance: '100px',
-            duration: 1000,
-            easing: 'ease-out',
-            viewFactor: 0.2,
-            mobile: false
-        });
-    </script>
+
 @endsection
 
 @section('content_js')
@@ -119,90 +111,131 @@
 
     </script>
     <script>
-        sr.reveal('.about .col-top .left', {
-            delay: 100,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.about .col-top .right', {
-            delay: 200,
-            interval: 300,
-            origin: 'right'
-        });
-        sr.reveal('.about .col-middle .left', {
-            delay: 100,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.about .col-middle .right', {
-            delay: 200,
-            interval: 300,
-            origin: 'right'
-        });
-        sr.reveal('.about .col-bottom', {
-            delay: 300
-        });
-        sr.reveal('.about .col-info', {
-            delay: 300
-        });
-        sr.reveal('.about .col-title', {
-            delay: 300
-        });
-        sr.reveal('.about .col-middle', {
-            delay: 300
-        });
-        sr.reveal('.about1 .tit', {
-            delay: 100
-        });
-        sr.reveal('.about1 .swiper-container', {
-            delay: 200,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.about2 .tit', {
-            delay: 100
-        });
-        sr.reveal('.about2 .swiper-slide', {
-            delay: 200,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.about3 .col-top', {
-            delay: 100
-        });
-        sr.reveal('.about3 .col-middle', {
-            delay: 200,
-            interval: 300,
-            origin: 'right'
-        });
-        sr.reveal('.about3 .col-bottom', {
-            delay: 300,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.about4 .tit', {
-            delay: 100
-        });
-        sr.reveal('.about4 .swiper-container', {
-            delay: 300,
-        });
-        sr.reveal('.contact .tit', {
-            delay: 100,
+        if($('div').hasClass('.about .col-top .left')) {
+            sr.reveal('.about .col-top .left', {
+                delay: 100,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.about .col-top .right')) {
+            sr.reveal('.about .col-top .right', {
+                delay: 200,
+                interval: 300,
+                origin: 'right'
+            });
+        }
+        if($('div').hasClass('.about .col-middle .left')) {
+            sr.reveal('.about .col-middle .left', {
+                delay: 100,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.about .col-middle .right')) {
+            sr.reveal('.about .col-middle .right', {
+                delay: 200,
+                interval: 300,
+                origin: 'right'
+            });
+        }
+        if($('div').hasClass('.about .col-bottom')) {
+            sr.reveal('.about .col-bottom', {
+                delay: 300
+            });
+        }
+        if($('div').hasClass('.about .col-info')) {
+            sr.reveal('.about .col-info', {
+                delay: 300
+            });
+        }
+        if($('div').hasClass('.about .col-title')) {
+            sr.reveal('.about .col-title', {
+                delay: 300
+            });
+        }
+        if($('div').hasClass('.about .col-middle')) {
+            sr.reveal('.about .col-middle', {
+                delay: 300
+            });
+        }
+        if($('div').hasClass('.about1 .tit')) {
+            sr.reveal('.about1 .tit', {
+                delay: 100
+            });
+        }
+        if($('div').hasClass('.about1 .swiper-container')) {
+            sr.reveal('.about1 .swiper-container', {
+                delay: 200,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.about2 .tit')) {
+            sr.reveal('.about2 .tit', {
+                delay: 100
+            });
+        }
+        if($('div').hasClass('.about2 .swiper-slide')) {
+            sr.reveal('.about2 .swiper-slide', {
+                delay: 200,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.about3 .col-top')) {
+            sr.reveal('.about3 .col-top', {
+                delay: 100
+            });
+        }
+        if($('div').hasClass('.about3 .col-middle')) {
+            sr.reveal('.about3 .col-middle', {
+                delay: 200,
+                interval: 300,
+                origin: 'right'
+            });
+        }
+        if($('div').hasClass('.about3 .col-bottom')) {
+            sr.reveal('.about3 .col-bottom', {
+                delay: 300,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.about4 .tit')) {
+            sr.reveal('.about4 .tit', {
+                delay: 100
+            });
+        }
+        if($('div').hasClass('.about4 .swiper-container')) {
+            sr.reveal('.about4 .swiper-container', {
+                delay: 300,
+            });
+        }
+        if($('div').hasClass('.contact .tit')) {
+            sr.reveal('.contact .tit', {
+                delay: 100,
+            });
+        }
+        if($('div').hasClass('.contact ul li')) {
+            sr.reveal('.contact ul li', {
+                delay: 200,
+                interval: 300,
+                origin: 'left'
+            });
+        }
+        if($('div').hasClass('.contact1 .layout .tit')) {
+            sr.reveal('.contact1 .layout .tit', {
+                delay: 100,
 
-        });
-        sr.reveal('.contact ul li', {
-            delay: 200,
-            interval: 300,
-            origin: 'left'
-        });
-        sr.reveal('.contact1 .layout .tit', {
-            delay: 100,
+            });
+        }
+        if($('div').hasClass('.contact1 .layout form')) {
+            sr.reveal('.contact1 .layout form', {
+                delay: 200,
 
-        });
-        sr.reveal('.contact1 .layout form', {
-            delay: 200,
-
-        });
+            });
+        }
 
     </script>
 @endsection
